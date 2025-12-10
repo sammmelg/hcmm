f28a374d1ecd36a028ba09a6f36d4fc045245fc3
shemmelg <shemmelgarn@lowell.edu>
2025-12-10 13:00:52 -0700

    Added file path and save filename as a command line argument. Removed peak & end atmospheric density and energy received calculations. Those features are not used in the model.

--- gmn_data_converter.py
+++ gmn_data_converter.py
@@ -23,88 +23,44 @@ def compute_row(idx, row, zR_val):
         begin = ad.getAtmDensity(np.radians(row['LatBeg']), np.radians(row['LonBeg']), row['HtBeg'] * 1000,
                                  row['Beginning_JD'])
 
-        # Peak atmospheric density
-        lat_peak = (np.radians(row['LatBeg']) + np.radians(row['LatEnd'])) / 2
-        lon_peak = (np.radians(row['LonBeg']) + np.radians(row['LonEnd'])) / 2
-        peak = ad.getAtmDensity(lat_peak, lon_peak, row['Peak_Ht'] * 1000,
-                                row['Beginning_JD'])
-
-        # End atmospheric density
-        end = ad.getAtmDensity(np.radians(row['LatEnd']), np.radians(row['LonEnd']), row['HtEnd'] * 1000,
-                               row['Beginning_JD'])
-
         # Beginning Energy received
         beg_ml_const = metsim.Constants()
         beg_ml_const.erosion_height_start = row['HtBeg'] * 1000
         beg_ml_const.v_init = row['Vinit'] * 1000
         beg_ml_const.zenith_angle = np.radians(zR_val)
         beg_ml_const.m_init = row['Mass_kg']
-
         poly = ad.fitAtmPoly(np.radians(row['LatBeg']), np.radians(row['LonBeg']),
                              beg_ml_const.erosion_height_start, beg_ml_const.h_init, row['Beginning_JD'])
         beg_ml_const.dens_co = poly
-
         beg_energy = metsim.energyReceivedBeforeErosion(beg_ml_const)[0] / 1e6
 
-        # Peak Energy received
-        peak_ml_const = metsim.Constants()
-        peak_ml_const.erosion_height_start = row['Peak_Ht'] * 1000
-        peak_ml_const.v_init = ((row['Vinit'] + row['Vavg']) / 2) * 1000
-        peak_ml_const.zenith_angle = np.radians(zR_val)
-        peak_ml_const.m_init = row['Mass_kg']
-
-        poly = ad.fitAtmPoly(lat_peak, lon_peak,
-                             peak_ml_const.erosion_height_start, peak_ml_const.h_init, row['Beginning_JD'])
-        peak_ml_const.dens_co = poly
-
-        peak_energy = metsim.energyReceivedBeforeErosion(peak_ml_const)[0] / 1e6
-
-        # End Energy received
-        end_ml_const = metsim.Constants()
-        end_ml_const.erosion_height_start = row['HtEnd'] * 1000
-        end_ml_const.v_init = row['Vavg'] * 1000
-        end_ml_const.zenith_angle = np.radians(zR_val)
-        end_ml_const.m_init = row['Mass_kg']
-
-        poly = ad.fitAtmPoly(np.radians(row['LatEnd']), np.radians(row['LonEnd']),
-                             end_ml_const.erosion_height_start, end_ml_const.h_init, row['Beginning_JD'])
-        end_ml_const.dens_co = poly
-
-        end_energy = metsim.energyReceivedBeforeErosion(end_ml_const)[0] / 1e6
-
-        return idx, begin, peak, end, beg_energy, peak_energy, end_energy
+        return idx, begin, beg_energy
 
     except Exception as e:
         print(f"Error at index {idx}: {e}")
-        return idx, np.nan, np.nan, np.nan, np.nan
+        return idx, np.nan, np.nan
 
 
 if __name__ == "__main__":
     # Parse command line args
     parser = argparse.ArgumentParser()
-    parser.add_argument("-year", type=str, help="Year as a string")
-    parser.add_argument("-mininterval", type=float, default=0.1, help="Minimum interval in seconds")
+    parser.add_argument("-path", type=str, help="File path to raw data")
+    parser.add_argument("-savefile", type=str, help="Filename for .csv of processed data")
     args = parser.parse_args()
 
     # Load data
-    year = args.year
-    meteor_df = pd.read_csv(f'/Users/shemmelg/grad_school/ms_class_ml/locams_testing/raw_locams_meteor_data.csv',
-                            low_memory=False)
+    meteor_df = pd.read_csv(args.path, low_memory=False)
     meteor_df = meteor_df[meteor_df.Beg_in.str.contains('True')]
     meteor_df = meteor_df[meteor_df.End_in.str.contains('True')]
 
-    print(f'The {year} dataset is {len(meteor_df)} entries long!')
-
     # Drop unnecessary parameters
     drop_params = ['IAU_number', 'Sollon', 'AppLST', 'Rageo', 'RAgeo_err', 'DECgeo', 'DECgeo_err', 'LAMgeo',
-                   'LAMgeo_err', 'BETgeo',
-                   'BETgeo_err', 'Vgeo', 'Vgeo_err', 'LAMhel', 'LAMhel_err', 'BEThel', 'BEThel_err', 'Vhel', 'Vhel_err',
-                   'a', 'a_err', 'e', 'e_err', 'i', 'i_err', 'peri', 'peri_err', 'node', 'node_err', 'Pi', 'Pi_err',
-                   'b',
-                   'b_err', 'q', 'q_err', 'f', 'f_err', 'M', 'M_err', 'Q', 'Q_err', 'n', 'n_err', 'T', 'T_err', 'Raapp',
-                   'RAapp_err', 'DECapp', 'DECapp_err', 'Qc', 'MedianFitErr', 'Num', 'Participating', 'TisserandJ_err',
-                   'Azim_E_err', 'Elev_err', 'Vinit_err', 'Vavg_err', 'LatBeg_err', 'LonBeg_err', 'HtBeg_err',
-                   'LatEnd_err',
+                   'LAMgeo_err', 'BETgeo', 'BETgeo_err', 'Vgeo', 'Vgeo_err', 'LAMhel', 'LAMhel_err', 'BEThel',
+                   'BEThel_err', 'Vhel', 'Vhel_err', 'a', 'a_err', 'e', 'e_err', 'i', 'i_err', 'peri', 'peri_err',
+                   'node', 'node_err', 'Pi', 'Pi_err', 'b', 'b_err', 'q', 'q_err', 'f', 'f_err', 'M', 'M_err',
+                   'Q', 'Q_err', 'n', 'n_err', 'T', 'T_err', 'Raapp', 'RAapp_err', 'DECapp', 'DECapp_err',
+                   'Qc', 'MedianFitErr', 'Num', 'Participating', 'TisserandJ_err', 'Azim_E_err', 'Elev_err',
+                   'Vinit_err', 'Vavg_err', 'LatBeg_err', 'LonBeg_err', 'HtBeg_err', 'LatEnd_err',
                    'LonEnd_err', 'HtEnd_err', 'Beg_in', 'End_in']
     meteor_df = meteor_df.drop(drop_params, axis=1)
 
@@ -115,33 +71,28 @@ if __name__ == "__main__":
     rows = list(meteor_df.iterrows())
     args_list = [(i, row, zR.loc[i]) for i, row in rows]
 
+    # Compute values that are necessary via parallel processing
     results = []
     with ProcessPoolExecutor() as executor:
         futures = [executor.submit(compute_row, i, row, zr) for i, row, zr in args_list]
 
         # Progress bar
         for future in tqdm(as_completed(futures), total=len(futures), desc="Processing rows",
-                           mininterval=args.mininterval):
+                           mininterval=10.0):
             results.append(future.result())
 
     # Sort and unpack results
     results.sort()
-    _, begin_atmDens, peak_atmDens, end_atmDens, begin_energy_rec, peak_energy_rec, end_energy_rec = zip(*results)
+    _, begin_atmDens, begin_energy_rec = zip(*results)
 
     # Post-process
-    meteor_df.insert(10, 'Deceleration', meteor_df.Vinit - meteor_df.Vavg)
+    meteor_df.insert(10, 'Deceleration', (meteor_df.Vinit - meteor_df.Vavg) / meteor_df.Duration)
     meteor_df.insert(16, 'AtmDensBeg', begin_atmDens)
-    meteor_df.insert(17, 'AtmDensPeak', peak_atmDens)
-    meteor_df.insert(18, 'AtmDensEnd', end_atmDens)
-    meteor_df.insert(20, 'TrailLength', meteor_df.Duration * meteor_df.Vavg)
-    meteor_df.insert(21, 'EnergyRecBeg', begin_energy_rec)
-    meteor_df.insert(22, 'EnergyRecPeak', peak_energy_rec)
-    meteor_df.insert(23, 'EnergyRecEnd', end_energy_rec)
-    meteor_df.insert(24, 'ZenithAngle', zR)
+    meteor_df.insert(17, 'TrailLength', meteor_df.Duration * meteor_df.Vavg)
+    meteor_df.insert(18, 'EnergyRecBeg', begin_energy_rec)
+    meteor_df.insert(19, 'ZenithAngle', zR)
 
-    # meteor_df.loc[meteor_df.Deceleration < 0, 'Deceleration'] = np.nan
-    # meteor_df.loc[meteor_df.Mass_kg <= 0, 'Mass_kg'] = np.nan
-
+    # Drop remaining unnecessary columns
     meteor_df = meteor_df.drop(['TisserandJ', 'Beginning_JD', 'Azim_E', 'Beginning', 'LatBeg',
                                 'LonBeg', 'LatEnd', 'LonEnd', 'Peak_Ht'], axis=1)
 
@@ -155,5 +106,4 @@ if __name__ == "__main__":
     meteor_df['IAU_code'] = iau_code
 
     # Save
-    meteor_df.to_csv(f'/Users/shemmelg/grad_school/ms_class_ml/locams_testing/'
-                     f'{year}_gmn_labeled_meteor_data_energy_wNum_PARALLELIZED.csv', index=False)
+    meteor_df.to_csv(args.savefile, index=False)
