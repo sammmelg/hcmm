49b7491d14e80bbc95ab47a2833e77dcd715cda4
shemmelg <shemmelgarn@lowell.edu>
2025-12-10 14:08:02 -0700

    Add documentation for each function.

--- gmn_data_converter.py
+++ gmn_data_converter.py
@@ -5,12 +5,26 @@ import WesternMeteorPyLib.wmpl.MetSim.MetSimErosion as metsim
 import argparse
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from tqdm import tqdm
-import warnings
-
-warnings.filterwarnings('ignore')
 
 
 def calc_kb(beg_atmos_dens, initial_vel, zenith_dist):
+    """
+    Compute Kb value for each row in the dataframe from using equation in Ceplecha 1988.
+
+    Parameters
+    ----------
+    beg_atmos_dens : pandas.Series
+        Meteor's beginning atmospheric density (gm/cm^3).
+    initial_vel : pandas.Series
+        Meteor's initial velocity (cm/sec).
+    zenith_dist : pandas.Series
+        Meteor's zenith angle (degrees).
+
+    Returns
+    -------
+    kb_values : pandas.Series
+        Kb value for each row in the dataframe, offset by -0.10 (Cordonnier et al. (2024)).
+    """
     kb_values = (np.log10(beg_atmos_dens) + (2.5 * np.log10(initial_vel)) -
                  (0.5 * np.log10(np.cos(np.radians(zenith_dist)))))
 
@@ -18,12 +32,34 @@ def calc_kb(beg_atmos_dens, initial_vel, zenith_dist):
 
 
 def compute_row(idx, row, zR_val):
-    try:
-        # Beginning atmospheric density
-        begin = ad.getAtmDensity(np.radians(row['LatBeg']), np.radians(row['LonBeg']), row['HtBeg'] * 1000,
-                                 row['Beginning_JD'])
+    """
+    Compute beginning atmospheric density and beginning energy
+    received for each row in the dataframe using wmpl.
 
-        # Beginning Energy received
+    Parameters
+    ----------
+    idx : int
+        Row index in the dataframe. Used to put results back in the same order as the original dataframe.
+    row : pandas.Series
+        A single row of values from the dataframe.
+    zR_val : pandas.Series
+        Meteor's zenith angle (degrees).
+
+    Returns
+    -------
+    idx : pandas.Series
+        Kb value for each row in the dataframe, offset by -0.10 (Cordonnier et al. (2024)).
+    beg_atmos_dens : pandas.Series
+        Meteor's beginning atmospheric density (kg/m^3).
+    beg_energy : pandas.Series
+        Meteor's energy received before ablation (MJ/m^2).
+    """
+    try:
+        # Beginning atmospheric density (Height: meters)
+        beg_atmos_dens = ad.getAtmDensity(np.radians(row['LatBeg']), np.radians(row['LonBeg']),
+                                               row['HtBeg'] * 1000, row['Beginning_JD'])
+
+        # Beginning Energy received (Heights: meters, Velocity: m/s)
         beg_ml_const = metsim.Constants()
         beg_ml_const.erosion_height_start = row['HtBeg'] * 1000
         beg_ml_const.v_init = row['Vinit'] * 1000
@@ -34,7 +70,7 @@ def compute_row(idx, row, zR_val):
         beg_ml_const.dens_co = poly
         beg_energy = metsim.energyReceivedBeforeErosion(beg_ml_const)[0] / 1e6
 
-        return idx, begin, beg_energy
+        return idx, beg_atmos_dens, beg_energy
 
     except Exception as e:
         print(f"Error at index {idx}: {e}")
@@ -50,8 +86,6 @@ if __name__ == "__main__":
 
     # Load data
     meteor_df = pd.read_csv(args.path, low_memory=False)
-    meteor_df = meteor_df[meteor_df.Beg_in.str.contains('True')]
-    meteor_df = meteor_df[meteor_df.End_in.str.contains('True')]
 
     # Drop unnecessary parameters
     drop_params = ['IAU_number', 'Sollon', 'AppLST', 'Rageo', 'RAgeo_err', 'DECgeo', 'DECgeo_err', 'LAMgeo',
@@ -65,7 +99,7 @@ if __name__ == "__main__":
     meteor_df = meteor_df.drop(drop_params, axis=1)
 
     # Zenith angles
-    zR = 90 - meteor_df.Elev
+    zR = 90 - meteor_df['Elev']
 
     # Prepare args for each row
     rows = list(meteor_df.iterrows())
@@ -86,9 +120,9 @@ if __name__ == "__main__":
     _, begin_atmDens, begin_energy_rec = zip(*results)
 
     # Post-process
-    meteor_df.insert(10, 'Deceleration', (meteor_df.Vinit - meteor_df.Vavg) / meteor_df.Duration)
+    meteor_df.insert(10, 'Deceleration', (meteor_df['Vinit'] - meteor_df['Vavg']) / meteor_df['Duration'])
     meteor_df.insert(16, 'AtmDensBeg', begin_atmDens)
-    meteor_df.insert(17, 'TrailLength', meteor_df.Duration * meteor_df.Vavg)
+    meteor_df.insert(17, 'TrailLength', meteor_df['Duration'] * meteor_df['Vavg'])
     meteor_df.insert(18, 'EnergyRecBeg', begin_energy_rec)
     meteor_df.insert(19, 'ZenithAngle', zR)
 
@@ -97,8 +131,8 @@ if __name__ == "__main__":
                                 'LonBeg', 'LatEnd', 'LonEnd', 'Peak_Ht'], axis=1)
 
     # Kb
-    init_vel = meteor_df.Vinit * 1e5
-    kb = calc_kb(meteor_df.AtmDensBeg / 1000, init_vel, zR)
+    init_vel = meteor_df['Vinit'] * 1e5
+    kb = calc_kb(meteor_df['AtmDensBeg'] / 1e3, init_vel, zR)
     meteor_df.insert(meteor_df.shape[1], 'Kb', kb)
 
     # Add IAU code to the dataframe
